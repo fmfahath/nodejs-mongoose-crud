52ac332f8c52f10bff12de902dee509b0e807ffe
fmfahath <fmfahath.dev@gmail.com>
2024-06-11 02:26:43 +0530

    connecting databse using mangoose

--- db.js
+++ db.js
@@ -1,17 +1,33 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
-const ObjectID = mongodb.ObjectId;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectId;
 
-let database;
+// let database;
+
+// async function getDatabase() {
+//     const client = await MongoClient.connect('mongodb://localhost:27017');
+//     database = client.db('library')
+
+//     if (!database) {
+//         console.log("Database not connected");
+//     }
+//     return database
+// }
+
+// module.exports = { getDatabase, ObjectID }
+
+// --------------------------------------------------------------------
+
+const mongoose = require('mongoose')
 
 async function getDatabase() {
-    const client = await MongoClient.connect('mongodb://localhost:27017');
-    database = client.db('library')
-
-    if (!database) {
-        console.log("Database not connected");
-    }
-    return database
+    mongoose.connect('mongodb://127.0.0.1:27017/library')
+        .then(() => {
+            console.log("Database connected!")
+        })
+        .catch((error) => {
+            console.log("Databse connection error: ", error.message);
+        })
 }
 
-module.exports = { getDatabase, ObjectID }
+module.exports = getDatabase;
