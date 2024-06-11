b8bba4ed08947054088fd8a0d44862ac2edf4926
fmfahath <fmfahath.dev@gmail.com>
2024-06-11 10:07:38 +0530

    edit, delete, update  operation done using mongoose

--- app.js
+++ app.js
@@ -45,14 +45,16 @@ app.get('/', async (req, res) => {
     if (req.query.edit_id) {
         edit_id = req.query.edit_id;
 
-        edit_book = await collection.findOne({ _id: new ObjectID(edit_id) })
+        // edit_book = await collection.findOne({ _id: new ObjectID(edit_id) })
+        edit_book = await BookModel.findOne({ _id: edit_id })
     }
 
     //delete
     if (req.query.delete_id) {
         delete_id = req.query.delete_id;
 
-        await collection.deleteOne({ _id: new ObjectID(delete_id) })
+        // await collection.deleteOne({ _id: new ObjectID(delete_id) })
+        await BookModel.deleteOne({ _id: delete_id })
         return res.redirect('/?status=3');
     }
 
@@ -88,12 +90,14 @@ app.post('/store_book', async (req, res) => {
 
 //update
 app.post('/update_book/:edit_id', async (req, res) => {
-    let book = { title: req.body.title, author: req.body.author };
+    // let book = { title: req.body.title, author: req.body.author };
     let edit_id = req.params.edit_id;
 
-    let database = await dbo.getDatabase();
-    const collection = database.collection('books')
-    await collection.updateOne({ _id: new ObjectID(edit_id) }, { $set: book })
+    // let database = await dbo.getDatabase();
+    // const collection = database.collection('books')
+    // await collection.updateOne({ _id: new ObjectID(edit_id) }, { $set: book })
+
+    await BookModel.findOneAndUpdate({ _id: edit_id }, { title: req.body.title, author: req.body.author })
     return res.redirect('/?status=2');
 })
 
